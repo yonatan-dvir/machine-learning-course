fe3bc01ded8dad26779234b63e4ded5b3b0269df
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-04-05 23:40:33 +0300

    Started working on the Linear Regression HW - Implemnted preprcess, bias trick and the compute cost functions

---
+++ hw1/hw1.py
@@ -0,0 +1,254 @@
+###### Your ID ######
+# ID1: 123456789
+# ID2: 987654321
+#####################
+
+# imports 
+import numpy as np
+import pandas as pd
+
+def preprocess(X,y):
+    """
+    Perform mean normalization on the features and true labels.
+
+    Input:
+    - X: Input data (m instances over n features).
+    - y: True labels (m instances).
+
+    Returns:
+    - X: The mean normalized inputs.
+    - y: The mean normalized labels.
+    """
+    # Calculate the mean (average) of the X,y arrays
+    mean_X = np.mean(X)
+    mean_y = np.mean(y)
+
+    # Calculate the maximum of the X,y arrays
+    max_X = np.max(X)
+    max_y = np.max(y)
+
+    # Calculate the minimum of the X,y arrays
+    min_X = np.min(X)
+    min_y = np.min(y)
+
+    # Normalize X and y
+    X = (X - mean_X) / (max_X-min_X)
+    y = (y - mean_y) / (max_y-min_y)
+
+    return X, y
+
+def apply_bias_trick(X):
+    """
+    Applies the bias trick to the input data.
+
+    Input:
+    - X: Input data (m instances over n features).
+
+    Returns:
+    - X: Input data with an additional column of ones in the
+        zeroth position (m instances over n+1 features).
+    """
+    # set m to be number of X rows (number of instances) and n to be number of X columns (number of features)
+    m = X.shape[0]
+
+    # Create an array of m arrays of 1 in each one of them
+    ones_column = np.ones((m, 1))
+
+    # Reshape X to be the same structure of ones_column, so we can stack the arrays in sequence horizontally
+    X = X.reshape(-1, 1)
+
+    # Set the ones column as the zeroth column of the features (Insert it at the beginning of X)
+    X = np.hstack((ones_column, X))
+
+    return X
+
+def compute_cost(X, y, theta):
+    """
+    Computes the average squared difference between an observation's actual and
+    predicted values for linear regression.  
+
+    Input:
+    - X: Input data (m instances over n features).
+    - y: True labels (m instances).
+    - theta: the parameters (weights) of the model being learned.
+
+    Returns:
+    - J: the cost associated with the current set of parameters (single number).
+    """
+    
+    J = 0  # We use J for the cost.
+    m = X.shape[0]  # Number of instances
+    # Calculate J with the cost function
+    for i in range(1, m):
+        h_theta_x_i = theta[0] + theta[1]*X[i-1][1]
+        y_i = y[i-1]
+        J += (h_theta_x_i - y_i) ** 2
+    J /= 2*m
+
+    return J
+
+def gradient_descent(X, y, theta, alpha, num_iters):
+    """
+    Learn the parameters of the model using gradient descent using 
+    the training set. Gradient descent is an optimization algorithm 
+    used to minimize some (loss) function by iteratively moving in 
+    the direction of steepest descent as defined by the negative of 
+    the gradient. We use gradient descent to update the parameters
+    (weights) of our model.
+
+    Input:
+    - X: Input data (m instances over n features).
+    - y: True labels (m instances).
+    - theta: The parameters (weights) of the model being learned.
+    - alpha: The learning rate of your model.
+    - num_iters: The number of updates performed.
+
+    Returns:
+    - theta: The learned parameters of your model.
+    - J_history: the loss value for every iteration.
+    """
+    
+    theta = theta.copy() # optional: theta outside the function will not change
+    J_history = [] # Use a python list to save the cost value in every iteration
+    ###########################################################################
+    # TODO: Implement the gradient descent optimization algorithm.            #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return theta, J_history
+
+def compute_pinv(X, y):
+    """
+    Compute the optimal values of the parameters using the pseudoinverse
+    approach as you saw in class using the training set.
+
+    #########################################
+    #### Note: DO NOT USE np.linalg.pinv ####
+    #########################################
+
+    Input:
+    - X: Input data (m instances over n features).
+    - y: True labels (m instances).
+
+    Returns:
+    - pinv_theta: The optimal parameters of your model.
+    """
+    
+    pinv_theta = []
+    ###########################################################################
+    # TODO: Implement the pseudoinverse algorithm.                            #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return pinv_theta
+
+def efficient_gradient_descent(X, y, theta, alpha, num_iters):
+    """
+    Learn the parameters of your model using the training set, but stop 
+    the learning process once the improvement of the loss value is smaller 
+    than 1e-8. This function is very similar to the gradient descent 
+    function you already implemented.
+
+    Input:
+    - X: Input data (m instances over n features).
+    - y: True labels (m instances).
+    - theta: The parameters (weights) of the model being learned.
+    - alpha: The learning rate of your model.
+    - num_iters: The number of updates performed.
+
+    Returns:
+    - theta: The learned parameters of your model.
+    - J_history: the loss value for every iteration.
+    """
+    
+    theta = theta.copy() # optional: theta outside the function will not change
+    J_history = [] # Use a python list to save the cost value in every iteration
+    ###########################################################################
+    # TODO: Implement the efficient gradient descent optimization algorithm.  #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return theta, J_history
+
+def find_best_alpha(X_train, y_train, X_val, y_val, iterations):
+    """
+    Iterate over the provided values of alpha and train a model using 
+    the training dataset. maintain a python dictionary with alpha as the 
+    key and the loss on the validation set as the value.
+
+    You should use the efficient version of gradient descent for this part. 
+
+    Input:
+    - X_train, y_train, X_val, y_val: the training and validation data
+    - iterations: maximum number of iterations
+
+    Returns:
+    - alpha_dict: A python dictionary - {alpha_value : validation_loss}
+    """
+    
+    alphas = [0.00001, 0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 2, 3]
+    alpha_dict = {} # {alpha_value: validation_loss}
+    ###########################################################################
+    # TODO: Implement the function and find the best alpha value.             #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return alpha_dict
+
+def forward_feature_selection(X_train, y_train, X_val, y_val, best_alpha, iterations):
+    """
+    Forward feature selection is a greedy, iterative algorithm used to 
+    select the most relevant features for a predictive model. The objective 
+    of this algorithm is to improve the model's performance by identifying 
+    and using only the most relevant features, potentially reducing overfitting, 
+    improving accuracy, and reducing computational cost.
+
+    You should use the efficient version of gradient descent for this part. 
+
+    Input:
+    - X_train, y_train, X_val, y_val: the input data without bias trick
+    - best_alpha: the best learning rate previously obtained
+    - iterations: maximum number of iterations for gradient descent
+
+    Returns:
+    - selected_features: A list of selected top 5 feature indices
+    """
+    selected_features = []
+    #####c######################################################################
+    # TODO: Implement the function and find the best alpha value.             #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return selected_features
+
+def create_square_features(df):
+    """
+    Create square features for the input data.
+
+    Input:
+    - df: Input data (m instances over n features) as a dataframe.
+
+    Returns:
+    - df_poly: The input data with polynomial features added as a dataframe
+               with appropriate feature names
+    """
+
+    df_poly = df.copy()
+    ###########################################################################
+    # TODO: Implement the function to add polynomial features                 #
+    ###########################################################################
+    pass
+    ###########################################################################
+    #                             END OF YOUR CODE                            #
+    ###########################################################################
+    return df_poly
